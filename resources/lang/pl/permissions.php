31f00a3fd2d69c813dc8b7342f273dbb4bb783a0
Maciej Rymarz <59456825+mako321@users.noreply.github.com>
2022-11-18 16:43:06 +0100

    Add translations (#14)

---
+++ resources/lang/pl/permissions.php
@@ -0,0 +1,12 @@
+<?php
+
+use EscolaLms\Vouchers\Enums\VoucherPermissionsEnum;
+
+return [
+    VoucherPermissionsEnum::COUPON_LIST => 'Lista voucherów',
+    VoucherPermissionsEnum::COUPON_CREATE => 'Utwórz voucher',
+    VoucherPermissionsEnum::COUPON_READ => 'Wyświetl voucher',
+    VoucherPermissionsEnum::COUPON_UPDATE => 'Aktualizuj voucher',
+    VoucherPermissionsEnum::COUPON_DELETE => 'Usuń voucher',
+    VoucherPermissionsEnum::COUPON_USE => 'Użyj voucher',
+];
